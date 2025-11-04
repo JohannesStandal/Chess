717461318355e41ef9f7d84d7f64d7a9872b2d2b
JohannesStandal <johanneshusevagstandal@gmail.com>
2025-11-04 22:03:34 +0100

    Added a class structure for zobrist hashing

---
+++ Zobrist_hashing/zobrist_hashing.js
@@ -0,0 +1,22 @@
+const zobrist_hash_values = []
+
+class zobrist_hashing {
+    constructor(){
+        this.hash = 0
+    }
+
+    createHash(){
+        this.hash = 0
+        return
+    }
+
+    removePiece(square, piece){
+
+    }
+
+    addPiece(square, piece){
+        
+    }
+
+    function
+}
