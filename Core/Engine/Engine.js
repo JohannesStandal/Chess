9389bf744729876b35b2c5b8ba480c6a27e1a1e5
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-02-10 13:34:01 +0100

    Pasted Engine description, and deleted old engine

--- Core/Engine/Engine.js
+++ Core/Engine/Engine.js
@@ -1,3 +1,35 @@
+/*
+Features
+
+Search
+- Negamax
+- Quiescence search
+- Alpha Beta pruning
+- Move order optimisation
+- Transposision table
+- Iterativ Deepening
+
+Evaluation
+- Material count
+- bonuses for certain squares for each piecetype
+- avoids threefold repetition
+
+TODO:
+
+10 - fix UI for promotion (currently breaks the entire program)
+
+50 - refactor the code to contain a chess engine folder
+50 - create match manager taking two folders for testing engines
+
+
+30 - Move encoder / decoder: (move represented by a 16 bit value)
+20 - Store bestmove in TT table
+80 - Opening book
+30 - Zobrist hashing should account for castling rights and en passant square
+15 - Track king squares incrementally
+30 - mobility bonus in evaluation
+*/
+
 import { Board } from "../Board/chessboard.js"
 import { Search } from "./Search.js"
 import { TimeManager } from "./TimeManager.js"
@@ -9,6 +41,10 @@ export class Engine {
         this.timeManager = new TimeManager(thinkingTime)
 
         this.search = new Search(this.board, this.timeManager)
+    }
+
+    Reset(){
+        this.board.Reset()
     }
 
     Bestmove(fen, moves){
