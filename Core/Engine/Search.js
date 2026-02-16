46d13e9ab77fccafa707a902f44024bb7fd7da88
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-02-16 19:26:22 +0100

    Fixed TT table hopefully

--- Core/Engine/Search.js
+++ Core/Engine/Search.js
@@ -4,8 +4,12 @@ import { MoveOrder } from "./MoveOrdering.js"
 import { QuiesenceSearch } from "./QuiesenceSearch.js"
 
 // constants
+const maxSearchDepth = 24
 const checkMateScore = 100000
+const mateTreshold = checkMateScore - maxSearchDepth
 const drawScore = -1
+var nodesSearched;
+
 
 export class Search {
     constructor(board, timeManager){
@@ -19,13 +23,18 @@ export class Search {
     IterativeDeepening(){
         // start searchtime
         this.timeManager.Start()
+        this.bestMove = null
 
         // iterative deepening
-        for (let depth = 1; depth < 24; depth++){
+        for (let depth = 1; depth < maxSearchDepth; depth++){
+            // tracking nodes too see improvement
+            nodesSearched = 0
+            
             // search the position
             this.currentDepth = depth
-            const score = this.Negamax(depth, -Infinity, Infinity)
-            if (checkMateScore <= score){
+            const score = this.Negamax(depth, 0, -Infinity, Infinity)
+            console.log(depth, score, nodesSearched)
+            if (mateTreshold <= score){
                 console.log("Found forced checkmate at M" + Math.ceil(this.currentDepth / 2))
                 this.timeManager.Stop()
             }
@@ -38,13 +47,38 @@ export class Search {
         return this.bestMove ?? this.board.GenerateLegalMoves()[0]
     }
 
-    Negamax(depth, alpha, beta){
+    Negamax(depth, ply, alpha, beta){
         if (this.timeManager.ExceededTimeLimit()){
             return null
         }
+        nodesSearched ++
         //Generer lovlege trekk
         const hash = this.board.zobrist.hash
+    
+        // generer trekk
+        const UnsortedlegalMoves = this.board.GenerateLegalMoves()
         
+        //Ingen lovlege trekk, sjekk etter sjakkmatt / sjakk patt
+        if (UnsortedlegalMoves.length == 0){    
+            //Checkmate
+            if (this.board.InCheck(this.board.white_To_Move)){
+                const mateScore = -(checkMateScore - ply)
+                this.TT.AddPosition(hash, mateScore, depth, "EXACT")
+                return mateScore
+    
+            }
+            
+            //Stalemate
+            this.TT.AddPosition(hash, drawScore, depth, "EXACT")
+            return drawScore
+        }
+    
+        // check for threefold repetition
+        if (ChessHelper.checkForRepetitions(this.board.repetitionTable)) {
+            //console.log("found threefold repetition in search")
+            return drawScore
+        }
+
         //transposition table
         if (this.TT.IsValidTransposition(hash, depth) && 
             depth != this.currentDepth){
@@ -52,6 +86,16 @@ export class Search {
             const entry = this.TT.table.get(hash)
             // TT position is accurate
             if (entry.flag == "EXACT"){
+                // Scale checmates to match depth
+                // a M2 found at 3ply -> M3
+                if (mateTreshold < entry.score){
+                    return entry.score + ply
+                }
+                // same but when losing
+                else if (-mateTreshold > entry.score){
+                    return entry.score - ply
+                }
+                // default
                 return entry.score
             }
             // update upper or lower bound
@@ -65,30 +109,6 @@ export class Search {
             if (alpha >= beta){
                 return entry.score
             }
-        }
-    
-        // generer trekk
-        const UnsortedlegalMoves = this.board.GenerateLegalMoves()
-        
-        //Ingen lovlege trekk, sjekk etter sjakkmatt / sjakk patt
-        if (UnsortedlegalMoves.length == 0){    
-            //Checkmate
-            if (this.board.InCheck(this.board.white_To_Move)){
-                const mateScore = -(checkMateScore + depth)
-                this.TT.AddPosition(hash, mateScore, depth, "EXACT")
-                return mateScore
-    
-            }
-            
-            //Stalemate
-            this.TT.AddPosition(hash, drawScore, depth, "EXACT")
-            return drawScore
-        }
-    
-            // check for threefold repetition
-        if (ChessHelper.checkForRepetitions(this.board.repetitionTable)) {
-            //console.log("found threefold repetition in search")
-            return drawScore
         }
     
         
@@ -112,7 +132,7 @@ export class Search {
             
             // Evaluate future position
             this.board.Make_Move(move)
-            const score = - this.Negamax(depth - 1, -beta, -alpha)
+            const score = - this.Negamax(depth - 1, ply + 1, -beta, -alpha)
             this.board.Unmake_Move(move)
             
             //exit point for iterative deepening
