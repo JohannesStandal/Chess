840d410acd670bdc808a4a4dd417404908dde206
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-02-16 22:51:09 +0100

    Moved Zobrist hashing

--- Core/Board/zobrist_hashing.js
+++ Core/Board/zobrist_hashing.js
@@ -1,4 +1,4 @@
-import { zobrist_hash_values, hash_white_to_move} from "../../Constants/zobrist_hash_values.js"
+import { zobrist_hash_values, hash_white_to_move} from "../../Core/Constants/zobrist_hash_values.js"
 import { Piece } from "./piece.js"
 import { Move } from "./piece.js"
 
