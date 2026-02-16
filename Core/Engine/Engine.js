04c4512f5ac2f8f40020f01cc4351101d3e7022e
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-02-16 22:51:58 +0100

    Preperations for UCI engine

--- Core/Engine/Engine.js
+++ Core/Engine/Engine.js
@@ -47,14 +47,16 @@ export class Engine {
         this.board.Reset()
     }
 
-    Bestmove(fen, moves){
-        // load fen
+    SetPosition(fen){
         this.board.Load_Fen(fen)
-        // make all moves on the board
+    }
+
+    MakeMoves(moves){
         for (let move of moves){
             this.board.Make_Move(move)
         }
-        // search
+    }
+    Bestmove(){
         return this.search.IterativeDeepening()
     }
 
--- Core/Engine/Search.js
+++ Core/Engine/Search.js
@@ -33,14 +33,14 @@ export class Search {
             // search the position
             this.currentDepth = depth
             const score = this.Negamax(depth, 0, -Infinity, Infinity)
-            console.log("depth: ", depth, " Nodes: ", nodesSearched)
+            // console.log("depth: ", depth, " Nodes: ", nodesSearched)
             if (mateTreshold <= score){
-                console.log("Found forced checkmate at M" + Math.ceil(this.currentDepth / 2))
+                // console.log("Found forced checkmate at M" + Math.ceil(this.currentDepth / 2))
                 this.timeManager.Stop()
             }
             // stop search if time limit is exceeded
             if (this.timeManager.cancelSearch){
-                console.log("Search canceled", this.currentDepth)
+                // console.log("Search canceled", this.currentDepth)
                 break
             }
         }
