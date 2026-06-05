46753f8a7d545999268ca15dc31162640e74f02a
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-06-05 20:57:44 +0200

    Opening Book

---
+++ Core/Openings/Openings.js
@@ -0,0 +1,50 @@
+// Import data
+import { Board } from "../Board/chessboard.js";
+import { Move } from "../Board/move.js";
+
+import rawOpeningLines from "../Openings/rawOpeningLines.json" with { type: "json" };
+import fs from "fs";
+
+
+const openingBook = {}
+
+const board = new Board()
+
+for (const line of rawOpeningLines){
+    board.Load_Fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    for (const UCImove of line){
+        const hash = board.zobrist.hash
+        const move = Move.UCItoUINT16(UCImove, board)
+        addMoveToBook(hash, move)
+        board.Make_Move(move)
+    }
+}
+// write to JSON
+fs.writeFileSync(
+    "openingBook.json",
+    JSON.stringify(openingBook, null, 2)
+)
+
+function addMoveToBook(hash, move){
+    // Check if the entry already exists 
+    const entry = openingBook[hash]
+    
+    // First time this position is seen
+    if (entry == null){
+        openingBook[hash] = []
+        openingBook[hash].push([move, 1])
+        return
+    }
+
+    // We have seen this positon before
+    for (const variations of entry){
+        // If we have seen this move before increment the counter
+        if (variations[0] == move){
+            variations[1] ++
+            return
+        }
+    }
+    // Add the move to the positon
+    entry.push([move, 1])
+}
+
--- Core/UCI_Engine/UCI.js
+++ Core/UCI_Engine/UCI.js
@@ -80,7 +80,7 @@ export class UCI {
       // fetch the list of UCI moves and make them on the board
       const UCIMoves = parts.slice(movesIndex + 1, parts.length)
       for (const UCImove of UCIMoves){
-        const move = decodeMove(UCImove, this.engine.board)
+        const move = Move.UCItoUINT16(UCImove, this.engine.board)
         this.engine.board.Make_Move(move)
       }
     }
@@ -127,55 +127,6 @@ export class UCI {
   send(msg) {
     process.stdout.write(msg + "\n");
   }
-}
-
-function decodeMove(UCIMove, board){
-  const parts = UCIMove.split("")
-
-  const letterToNumber = {
-    "a": 0,
-    "b": 1,
-    "c": 2,
-    "d": 3,
-    "e": 4,
-    "f": 5,
-    "g": 6,
-    "h": 7,
-  }
-
-  const start = letterToNumber[parts[0]] + (parseInt(parts[1]) - 1)* 8
-  const target = letterToNumber[parts[2]] + (parseInt(parts[3]) - 1) * 8 
-  
-  const movedPiece = board.square[start]
-  const capturedPiece = board.square[target]
-
-  let flag = 0b0000
-  if (capturedPiece != 0){
-    flag |= Move.flags.captures
-  }
-
-  const isPawn = Piece.IsType(movedPiece, Piece.pawn)
-  const doublePush = (Math.abs(start - target) == 16)
-  if (isPawn && doublePush){
-    flag = Move.doublePush
-  }
-  
-  if (parts.length == 5){
-    const letterToFlag = {
-      "n": Move.flags.knightPromotion,
-      "b": Move.flags.bishopPromotion,
-      "r": Move.flags.rookPromotion,
-      "q": Move.flags.queenPromotion,
-    }
-
-    flag += letterToFlag[parts[4]]
-  }
-
-  if (UCIMove == "e1g1" || UCIMove == "e8g8") flag = Move.flags.kingCastle
-  if (UCIMove == "e1c1" || UCIMove == "e8c8") flag = Move.flags.queenCastle
-  if (target == board.enPassantSquare) flag = Move.flags.enPassant
-
-  return Move.EncodeUINT16(start, target, flag)
 }
 
 new UCI()
