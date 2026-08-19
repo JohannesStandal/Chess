b7e5780780f1d8fe627b7d924872f5b937070694
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-08-19 15:38:34 +0200

    Adding build.js

---
+++ Builder/build.cjs
@@ -0,0 +1,29 @@
+const esbuild = require('esbuild');
+const path = require('path');
+
+esbuild.build({
+  entryPoints: [path.resolve(__dirname, '../Core/UCI_Engine/UCI.js')],
+  bundle: true,
+  platform: 'node',
+  format: 'cjs',
+  outfile: path.resolve(__dirname, 'dist/engine.cjs'),
+  sourcemap: true,
+  minify: true,
+  drop: ["console"],
+  external: [
+    'readline',
+    'fs',
+    'path',
+    'os',
+    'child_process',
+    'events',
+    'stream'
+  ],
+}).catch(() => process.exit(1));
+
+/**
+ * Commands for building the engine.exe file.
+ * 
+ * 1: node Builder/build.cjs
+ * 2: pkg Builder/dist/engine.cjs --targets node18-win-x64 --output Builder/Builds/PrebenV2.exe
+ */
