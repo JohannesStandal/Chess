16d18337c2dff24efc733bb0065bb598c0df34c3
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-09-01 22:01:31 +0200

    Clean up

--- Core/Engine/Transposition_Table.js
+++ Core/Engine/Transposition_Table.js
@@ -1,33 +1,4 @@
-import { mateTreshold, checkMateScore } from "../Constants/SearchConstants.js"
-
-/**
- * 
- * TT.read()
- * 
- * - hit
- *   - Stored position has been searched deeper
- *       mate scaling
- *       return {all data}
- *      
- *   - Needs a deeper search
- *       return {no hit, bestmove}
- * 
- * - no hit
- * 
- * return {no hit, no bestmove}
- * 
- * 
- * 
- * TT.Store() 
- * 
- * - hit
- * 
- * - no hit
- * Store all data
- * 
- */
-
-
+import { mateTreshold } from "../Constants/SearchConstants.js"
 
 export class Transposition_Table {
     constructor (mb = 64){
@@ -39,35 +10,20 @@ export class Transposition_Table {
         const bytes = mb * 1024 * 1024
         this.numEntries = Math.floor(bytes / 31)
         
-
-        // // find power of two that makes the table big enough
-        // this.numEntries = 1
-        // while (this.numEntries < numEntries){
-        //    this.numEntries = this.numEntries << 1
-        // }
-        // this.numEntries = this.numEntries >> 1
-        // // this.numEntries = 1 << 20
-        // this.mask = this.numEntries - 1
-        
-
         // Initialize arrays
         this.hashHigh = new Array(this.numEntries) // 4 bytes
         this.hashLow  = new Array(this.numEntries) // 4 bytes
 
         this.bestMove = new Array(this.numEntries) // 2 bytes
-        this.depth = new Array(this.numEntries).fill(-1)     // 1 bytes
-        this.score = new Array(this.numEntries)    // 4 bytes 
+        this.depth = new Array(this.numEntries).fill(-1) // 1 byte
+        this.score = new Array(this.numEntries)          // 4 bytes 
         this.flag = new Array(this.numEntries)           // 16 byte
-
-        this.collisions = 0
-
     }
 
     Read(high, low, ply, currentDepth){
         if (this.disabled) return {"hit": false, "move": null}
 
         const index = (high % this.numEntries)
-        // console.log(index)
 
         // Relevant to see if entry is useful
         const ttHigh = this.hashHigh[index]
@@ -103,13 +59,10 @@ export class Transposition_Table {
 
         const index = (high % this.numEntries)
        
-        // this part is temporarily disabled for debugging
-
         // Relevant to see if entry is useful
         const ttHigh = this.hashHigh[index]
         const ttLow = this.hashLow[index]
         const storedDepth = this.depth[index]
-        const storedScore = this.score[index]
         
         // // Entry already exists. Should it be overwritten
         if (ttHigh == high && ttLow == low){
